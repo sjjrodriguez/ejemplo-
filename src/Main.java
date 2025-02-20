b1a4a4f9b3e0b90cec3f7fef0474ec34e79cdb65
sjjrodriguez <sjrodriguez120@gmail.com>
2025-02-20 16:11:04 -0500

    revert

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("ejemplo Git");
+        System.out.println("ejemplo ramas");
+        System.out.println("hola");
     }
 }
